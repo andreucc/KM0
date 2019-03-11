1e68da6ed004c4ef9f4062771446b4358c9dce5f
xavivax1 <xavier.deviala@gmail.com>
2019-03-11 16:25:23 +0100

    curtain + styling

--- public/javascripts/curtain.js
+++ public/javascripts/curtain.js
@@ -1,8 +1,8 @@
-const wrapper = document.getElementById('wrapper');
-const menuTrigger = document.getElementById('menu_trigger');
-const menu = document.getElementById('menu');
-menuTrigger.addEventListener('click', () => {
-  menuTrigger.classList.toggle('open');
-  menu.classList.toggle('open');
-  wrapper.classList.toggle('pushed');
-});
+
+function openNav () {
+  document.getElementById('myNav').style.width = '60%';
+}
+
+function closeNav () {
+  document.getElementById('myNav').style.width = '0%';
+}
